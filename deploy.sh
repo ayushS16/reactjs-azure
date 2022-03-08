a7dbc2b77a193b680eaaed7c01c04b386451704a
ayushS16 <ss.ayush16@gmail.com>
2022-03-08 13:40:38 +0530

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 #Create the artifacts bucket (this will store zipped lambda functions)
-aws s3api head-bucket --bucket ${artifacts_bucket} 2>/dev/null 
-if [ $? -ne 0 ]; then
-    echo "Artifacts bucket does not exist, creating bucket: ${artifacts_bucket}"
-    aws s3 mb s3://${artifacts_bucket}
-    if [ $? -ne 0 ]; then
-        echo "Could not create bucket ${artifacts_bucket}. Deployment aborted"
-        exit 1
-    fi
-fi
+aws s3 mb s3://my-react-s3 --region ap-south-1
